e7f88237e13029faa15f3ad5f403f3ff12dac370
azqeze <alexzhar.work@gmail.com>
2021-01-26 17:25:23 +0300

    Задание №2
    Не очень понятно задание, поэтому просто сделал вызываемый класс Point в котором запилил нужную функцию и его вызов из MyFirstProgram

--- sandbox/src/main/java/studypkg/MyFirstProgram.java
+++ sandbox/src/main/java/studypkg/MyFirstProgram.java
@@ -3,6 +3,10 @@ package studypkg;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println("Zdarova");
+        System.out.println();
+
+        Point p = new Point(1,2,3,4);
+        System.out.println("Расстояние между точкой A с координатами " + p.p1 + " и точкой B с координатами "+ p.p2 + " = " + p.distance());
     }
 }
---
+++ sandbox/src/main/java/studypkg/Point.java
@@ -0,0 +1,24 @@
+package studypkg;
+
+public class Point {
+
+    public double x1;
+    public double y1;
+    public double x2;
+    public double y2;
+    public String p1;
+    public String p2;
+
+
+    public Point(double x1, double y1, double x2, double y2) {
+        this.p1 = x1+";"+y1;
+        this.p2 = x2+";"+y2;
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+    public double distance() {
+        return Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
+    }
+}
