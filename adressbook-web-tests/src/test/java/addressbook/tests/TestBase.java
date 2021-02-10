b3f5e5c118c33b5ce2f137917c89d88aa5fcdf5b
azqeze <alexzhar.work@gmail.com>
2021-02-10 15:25:43 +0300

    Добавили возможность запуска теста в других браузерах

--- adressbook-web-tests/src/test/java/addressbook/tests/TestBase.java
+++ adressbook-web-tests/src/test/java/addressbook/tests/TestBase.java
@@ -1,14 +1,15 @@
 package addressbook.tests;
 
 import addressbook.appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-public class TestBase extends ApplicationManager {
+public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager();
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
-    @BeforeMethod(alwaysRun = true)
+    @BeforeMethod
     public void setUp() throws Exception {
         app.init();
     }
