d239d368b819286ec52288c282e4d61e4293dce9
azqeze <alexzhar.work@gmail.com>
2021-02-23 16:39:02 +0300

    Лекция 4.3
    Добавили проверку количества групп в тест на работу с группами

--- adressbook-web-tests/src/test/java/addressbook/tests/GroupCreationTest.java
+++ adressbook-web-tests/src/test/java/addressbook/tests/GroupCreationTest.java
@@ -1,6 +1,7 @@
 package addressbook.tests;
 
 import addressbook.model.GroupData;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class GroupCreationTest extends TestBase {
@@ -8,7 +9,10 @@ public class GroupCreationTest extends TestBase {
     @Test
     public void testGroupCreation() throws Exception {
         app.getNavigationHelper().gotoGroupPage();
+        int before = app.getGroupHelper().getGroupCount();
         app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+        int after = app.getGroupHelper().getGroupCount();
+        Assert.assertEquals(after, before + 1);
     }
 
 }
