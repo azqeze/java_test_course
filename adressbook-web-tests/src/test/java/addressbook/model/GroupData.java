cabaa7d9dc34b17a65e31fc59be3d6681a9b6ec2
azqeze <alexzhar.work@gmail.com>
2021-02-23 22:09:12 +0300

    Лекция 4,8 + 4,9
    Поиск максимального значения id для теста на создание группы

--- adressbook-web-tests/src/test/java/addressbook/model/GroupData.java
+++ adressbook-web-tests/src/test/java/addressbook/model/GroupData.java
@@ -3,16 +3,17 @@ package addressbook.model;
 import java.util.Objects;
 
 public class GroupData {
-    private final String id;
+
+    private int id;
     private final String name;
     private final String header;
     private final String footer;
 
-    public String getId() {
+    public int getId() {
         return id;
     }
 
-    public GroupData(String id, String name, String header, String footer) {
+    public GroupData(int id, String name, String header, String footer) {
         this.id = id;
         this.name = name;
         this.header = header;
@@ -20,10 +21,14 @@ public class GroupData {
     }
 
     public GroupData(String name, String header, String footer) {
-        this.id = null;
+        this.id = 0;
         this.name = name;
         this.header = header;
         this.footer = footer;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -43,7 +48,7 @@ public class GroupData {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         GroupData groupData = (GroupData) o;
-        return Objects.equals(id, groupData.id) && Objects.equals(name, groupData.name);
+        return id == groupData.id && Objects.equals(name, groupData.name);
     }
 
     @Override
@@ -54,7 +59,7 @@ public class GroupData {
     @Override
     public String toString() {
         return "GroupData{" +
-                "id='" + id + '\'' +
+                "id=" + id +
                 ", name='" + name + '\'' +
                 '}';
     }
